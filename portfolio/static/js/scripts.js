f8bafd781a7c353031e31986c2c57295a0d4afe2
st003 <stuart.j.calcote@gmail.com>
2024-06-10 15:41:44 -0700

    Updated olp project to use revised slideshow format

--- portfolio/static/js/scripts.js
+++ portfolio/static/js/scripts.js
@@ -5,92 +5,6 @@ const closeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
 
 const forwardSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16"><path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg>'
 
-// modal logic
-function openModal(modalID) {
-    document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
-    const modal = document.getElementById(modalID);
-    const modalContent = modal.getElementsByClassName('modalContent').item(0);
-
-    // construct close button
-    const closeBtn     = document.createElement('button');
-    closeBtn.innerHTML = closeSvg;
-    closeBtn.classList.add('modalExitButton');
-    closeBtn.addEventListener('click', () => { closeModal(modalID); });
-    modalContent.appendChild(closeBtn);
-
-    modal.style.display = 'flex';
-
-    function closeModal(modalID) {
-        document.getElementsByTagName('body')[0].style.overflowY = 'auto';
-        document.getElementById(modalID).style.display = 'none';
-        document.getElementsByClassName('modalExitButton').item(0).remove();
-    }
-}
-
-// slideshow logic
-function incrementSlide(slideshow, currentSlide, increment) {
-    return displaySlide(slideshow, currentSlide += increment);
-}
-
-function displaySlide(slideshow, slideNum) {
-    const counter = document.getElementById(`${slideshow.id}-counter`);
-    const slides  = slideshow.getElementsByClassName('slide');
-
-    // wrapping behavior
-    if (slideNum > slides.length - 1) slideNum = 0;
-    if (slideNum < 0) slideNum = slides.length -1;
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-
-    counter.innerHTML = `${slideNum + 1} / ${slides.length}`;
-    slides[slideNum].style.display  = 'block';
-
-    return slideNum;
-}
-
-function initSlideshow(slideshowID) {
-    const slideshow = document.getElementById(slideshowID);
-
-    // create nav
-    const slideshowNav = document.createElement('div');
-    slideshowNav.classList.add('slideshowNav');
-
-    const backBtn     = document.createElement('button');
-    backBtn.innerHTML = backSvg;
-    backBtn.classList.add('slideshowButton');
-    slideshowNav.appendChild(backBtn);
-
-    const slideCounterContainer = document.createElement('div');
-    slideCounterContainer.classList.add('slideCounterContainer');
-
-    const slideCounter = document.createElement('div');
-    slideCounter.id = `${slideshowID}-counter`;
-    slideCounter.classList.add('slideCounter');
-
-    slideCounterContainer.appendChild(slideCounter);
-    slideshowNav.appendChild(slideCounterContainer);
-
-    const fwdBtn     = document.createElement('button');
-    fwdBtn.innerHTML = forwardSvg;
-    fwdBtn.classList.add('slideshowButton');
-    slideshowNav.appendChild(fwdBtn);
-
-    // insert nav
-    slideshow.appendChild(slideshowNav);
-
-    const initialSlideNum = 0;
-    displaySlide(slideshow, initialSlideNum);
-
-    return {
-        slideshow: slideshow, 
-        slideNum: initialSlideNum,
-        backBtn: backBtn,
-        fwdBtn: fwdBtn
-    }
-}
-
 function beforeAfter(baCompareID) {
     const baCompare = document.getElementById(baCompareID);
     const beforeImg = baCompare.getElementsByClassName('before').item(0);
@@ -107,21 +21,6 @@ function beforeAfter(baCompareID) {
 
 /* EVENT LISTENERS */
 document.addEventListener('DOMContentLoaded', () => {
-
-    // configure slideshows
-    const slideshows = document.getElementsByClassName('slideshow');
-    for (let i = 0; i < slideshows.length; i++) {
-
-        const currentSlideshow = initSlideshow(slideshows[i].id);
-        let currentSlideNum = currentSlideshow.slideNum;
-        currentSlideshow.backBtn.addEventListener('click', () => {
-            currentSlideNum = incrementSlide(currentSlideshow.slideshow, currentSlideNum, -1)
-        });
-        currentSlideshow.fwdBtn.addEventListener('click', () => {
-            currentSlideNum = incrementSlide(currentSlideshow.slideshow, currentSlideNum, 1)
-        });
-
-    }
 
     // configure Xslideshows
     if (document.getElementsByClassName('XslideShowContainer')) {
